5a9638d611fe5feb4182958f00bc163c6b08a61c
SoumyaD27 <soumyad233@gmail.com>
2022-12-01 19:19:29 +0530

    [Soumya]-Prime Number

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.Write("Enter a Number : ");
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 2; i < n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                }
+                else
+                {
+                    Console.Write("Number is Prime.");
+                }
+            } 
+        }
+    }
+}
