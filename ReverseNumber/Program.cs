a7663394ca0f1f8e315ed45d23a508cddcb2ffe5
SoumyaD27 <soumyad233@gmail.com>
2022-12-01 22:44:57 +0530

    [Soumya]-Reversing the given number

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num, Rev, rem;
+            Console.WriteLine("Enter a No. to reverse: ");
+            Num = int.Parse(Console.ReadLine());
+            Rev = 0;
+            while (Num > 0)
+            {
+                rem = Num % 10;
+                Rev = (Rev * 10) + rem;
+                Num = Num / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}", Rev);
+            Console.ReadLine();
+        }
+    }
+}
