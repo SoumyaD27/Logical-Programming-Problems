5ce7496d1c9f5edbf241adee528fd5f2212cabbf
SoumyaD27 <soumyad233@gmail.com>
2022-12-01 23:24:25 +0530

    [Soumya]-Stimulate Stopwatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        int i;
+        // Create new stopwatch
+        Stopwatch stopwatch = new Stopwatch();
+        // Begin timing
+        stopwatch.Start();
+        for ( i = 0; i < 10; i++)
+        {
+            Console.WriteLine("HI");
+        }
+        // Stop timing
+        stopwatch.Stop();
+        Console.WriteLine("Time Elapsed : {0}",stopwatch.Elapsed);
+        Console.ReadLine();
+    }
+}
